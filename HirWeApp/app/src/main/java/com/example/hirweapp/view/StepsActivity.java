6448cc8ac69a61356249b9ba75d4da6c7e3c9d0d
Diana Valbuena <dcvalbuena@gmail.com>
2021-11-08 15:56:21 -0500

    Avances Pantallas

---
+++ HirWeApp/app/src/main/java/com/example/hirweapp/view/StepsActivity.java
@@ -0,0 +1,16 @@
+package com.example.hirweapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hirweapp.R;
+
+public class StepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps);
+    }
+}
